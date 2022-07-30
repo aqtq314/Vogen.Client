fbcd6b62d16e283bf02efc14f638a3a9d1035a6d
Xial <450326581@qq.com>
2022-07-30 22:36:07 +0800

    Drag to change Time signature

---
+++ Vogen.Client/Converters/XamlStringifiers.cs
@@ -0,0 +1,17 @@
+﻿using Doaz.Reactive;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace Vogen.Client.Converters
+{
+    public static class XamlStringifiers
+    {
+        public static IValueConverter TimeSignatureToString { get; } = ValueConverter.Create(
+          (TimeSignature timgSig) => timgSig.ToString(),
+          (string stringValue) => TimeSignature.Parse(stringValue));
+    }
+}
